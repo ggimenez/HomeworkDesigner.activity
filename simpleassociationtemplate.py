51d7f42a2deb307077c71d1e1108d91165502889
Gabriel A. Giménez <gabrielgimenez85@gmail.com>
2014-11-29 23:44:09 -0300

    ObjectChooser feature

--- simpleassociationtemplate.py
+++ simpleassociationtemplate.py
@@ -12,26 +12,134 @@ import pango
 import random
 
 from sugar.graphics.objectchooser import ObjectChooser
-
+from sugar.graphics.entry import CanvasEntry
 ''' Scales '''
 IMAGES_SCALE = [100, 100]
 LETTERS_SCALE = [100, 100]
 
 
-'''Color Selection association
-Reference of colours codes :http://www.rapidtables.com/web/color/RGB_Color.htm
-'''
-COLOURS_ASSOCIATION = []
-#medium sea green
-COLOURS_ASSOCIATION.append({"colour":gtk.gdk.Color("#3CB371"), "available":True})
-#teal
-COLOURS_ASSOCIATION.append({"colour":gtk.gdk.Color("#008080"), "available":True})
-#thistle
-COLOURS_ASSOCIATION.append({"colour":gtk.gdk.Color("#D8BFD8"), "available":True})
-#dark sea green
-COLOURS_ASSOCIATION.append({"colour":gtk.gdk.Color("#8FBC8F"), "available":True})
-#forest green
-COLOURS_ASSOCIATION.append({"colour":gtk.gdk.Color("#228B22"), "available":True})
+class ModalWindowSelectItem:
+
+	def __init__ (self, parent):
+		self.parent = parent
+		self.modalWindow = gtk.Window()
+		
+		self.frameWindowScrolled = gtk.Frame()
+		self.frameWindowScrolled.modify_bg(gtk.STATE_NORMAL, gtk.gdk.Color("black"))
+		
+		self.frameHBoxButton = gtk.Frame()
+		self.frameHBoxButton.modify_bg(gtk.STATE_NORMAL, gtk.gdk.Color("black"))
+			
+		self.scrolledWindow = gtk.ScrolledWindow()
+		
+		'''Disable interaction with parent'''
+		self.modalWindow.set_modal (True)
+
+		# Tell WM this is a dialog
+		self.modalWindow.set_type_hint (gtk.gdk.WINDOW_TYPE_HINT_DIALOG)
+
+		# Tell WM this window belongs to parent
+		self.modalWindow.set_transient_for (parent)
+
+		self.modalWindow.set_default_size(1000,500)
+		self.modalWindow.set_decorated(False)
+	
+		self.vBoxModalWindow = gtk.VBox(False, 0)
+		
+		self.hBoxItem =  gtk.HBox(True, 10)
+		
+		entry = gtk.Entry()
+		self.hBoxItem.pack_start(entry, False,False	,0)
+		
+		imagePlaceHolder = gtk.Image()
+		imagePlaceHolder.set_from_pixbuf(gtk.gdk.pixbuf_new_from_file("./images/img_place_holder.gif").scale_simple(300, 200, 2))
+		
+		
+		vBoxImageItem = gtk.VBox(True, 0)
+		
+		frameEventBoxImageItem = gtk.Frame()
+		frameEventBoxImageItem.modify_bg(gtk.STATE_NORMAL, gtk.gdk.Color("orange"))
+		
+		eventBoxImageItem = gtk.EventBox()
+		eventBoxImageItem.connect("button-press-event", self.imageItemSelected)
+		eventBoxImageItem.add(imagePlaceHolder)
+		
+		frameEventBoxImageItem.add(eventBoxImageItem)
+		
+		vBoxImageItem.pack_start(frameEventBoxImageItem, False,False,0)
+		self.hBoxItem.pack_start(vBoxImageItem, False,False	,0)
+		
+		imageButtonIcon = gtk.Image()
+		imageButtonIcon.set_from_stock(gtk.STOCK_CANCEL, gtk.ICON_SIZE_MENU)	
+		buttonCancel = gtk.Button()
+		buttonCancel.set_image(imageButtonIcon)
+		buttonCancel.set_label("Cancelar")
+		buttonCancel.connect ("clicked", self.cancelButtonCallBack)
+		buttonCancel.modify_bg(gtk.STATE_NORMAL, gtk.gdk.Color("black"))
+		
+		buttonOkIcon = gtk.Image()
+		buttonOkIcon.set_from_stock(gtk.STOCK_APPLY, gtk.ICON_SIZE_MENU)	
+		buttonOk = gtk.Button()
+		buttonOk.set_image(buttonOkIcon)
+		buttonOk.set_label("Aceptar")
+		#buttonOk.connect ("clicked", self.cancelButtonCallBack)
+		buttonOk.modify_bg(gtk.STATE_NORMAL, gtk.gdk.Color("black"))
+		
+		
+		
+		hBoxButton = gtk.HBox(True,0)
+		hBoxButton.pack_start(buttonOk, False,False	,0)
+		hBoxButton.pack_start(buttonCancel, False,False	,0)
+		
+		self.scrolledWindow.add_with_viewport(self.hBoxItem)
+		self.frameWindowScrolled.add(self.scrolledWindow)
+		self.vBoxModalWindow.pack_start(self.frameWindowScrolled, True,True,0)
+		
+		self.frameHBoxButton.add(hBoxButton)
+		self.vBoxModalWindow.pack_start(self.frameHBoxButton, False,False,0)
+		
+		self.modalWindow.add(self.vBoxModalWindow)
+	
+	def imageItemSelected(self, eventBox, *args):
+		chooser = ObjectChooser(parent=self.modalWindow, what_filter='Image')
+		result = chooser.run()
+		self.parent.getLogger().debug("chooser result :")
+		self.parent.getLogger().debug(result)
+		self.jobject = None
+		self.path = None
+		if result == gtk.RESPONSE_ACCEPT:
+			self.jobject = chooser.get_selected_object()
+			self.path = str(self.jobject.get_file_path())
+			'''self.parent.getLogger().debug(self.jobject)'''	
+			self.parent.getLogger().debug(self.path)
+			'''self.parent.getLogger().debug(self.jobject.get_metadata().keys())'''
+			'''self.parent.getLogger().debug(self.jobject.get_metadata().get('mime_type'))'''
+			
+		if self.path != None:
+			imageSelected = gtk.Image()
+			imageSelected.set_from_pixbuf(gtk.gdk.pixbuf_new_from_file(self.path).scale_simple(300, 300, 2))
+			oldImage = eventBox.get_children()[0]
+			eventBox.remove(oldImage)
+			eventBox.add(imageSelected)
+			eventBox.show_all()
+			
+		
+	def enterNotifyEventBoxCallBack(self, eventBox, *args):
+		eventBox.modify_bg(gtk.STATE_NORMAL, gtk.gdk.Color("orange"))
+	def leaveNotifyEventBoxCallBack(self, eventBox, *args):
+		eventBox.modify_bg(gtk.STATE_NORMAL, gtk.gdk.Color("gray")) 
+		
+	def cancelButtonCallBack(self, button,*args):
+		self.modalWindow.destroy()
+		
+	def exerciseTypeSelectedCallBack(self, eventBox, *args):
+		#self.parent._logger.debug(args)
+		codeExerciseType = args[1]['code']
+		self.modalWindow.destroy()
+		self.parent.createNewExerciseType(codeExerciseType)
+		
+	def show(self):
+		self.modalWindow.show_all ()
 
 
 '''Curren item selection association'''
@@ -62,7 +170,6 @@ class SimpleAssociationTemplate():
 			eventBoxOption.connect("button-press-event", self.itemSelectedCallBack)
 			self.addEventBoxToVBox(eventBoxOption, vBoxOptions)			
 			
-			
 			'''Correspondences'''
 			eventBoxCorrespondence = self.createEventBox()
 			eventBoxCorrespondence.connect("button_press_event", self.itemSelectedCallBack)
@@ -75,16 +182,15 @@ class SimpleAssociationTemplate():
 		vBoxWindows.pack_start(frameExercises, True,True,0)
 		
 		windowSimpleAssociation.add_with_viewport(vBoxWindows)
-		
-		#self.selectFirtImage(firstOptionEventBox)
-		
+				
 		return windowSimpleAssociation
 	
 	def itemSelectedCallBack(self, eventBox, *args):
 		self.mainWindows.getLogger().debug("Inside: itemSelectedCallBack")
-		chooser = ObjectChooser(parent=self.mainWindows, what_filter='Image')
-		result = chooser.run()
-	
+		#chooser = ObjectChooser(parent=self.mainWindows, what_filter='Image')
+		#result = chooser.run()
+		dialogInsertNewItem = ModalWindowSelectItem(self.mainWindows)
+		dialogInsertNewItem.show()
 	
 	def addEventBoxToVBox(self, eventBox, vBox):
 		frameEventBox = gtk.Frame() 
@@ -99,10 +205,7 @@ class SimpleAssociationTemplate():
 		eventBox.add(blankLabel)
 			
 		return eventBox
-			
-		
-	
-	
+				
 	def setAllAvailableSelectionColour(self):
 		for colour in COLOURS_ASSOCIATION:
 			colour['available'] = True
